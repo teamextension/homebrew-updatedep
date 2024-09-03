d3af5a38ba9335931088b6faa5ce4671d9682430
jesli6991 <119661452+jesli6991@users.noreply.github.com>
2024-09-04 01:36:11 +0800

    Update Updatedep.rb

--- Formula/Updatedep.rb
+++ Formula/Updatedep.rb
@@ -1,8 +1,8 @@
 class Updatedep < Formula
   desc "Updatedep is an application that analyzes the dependencies of a project, this helps check what dependency versions can be used with less to no vulnerabilities."
   homepage "https://github.com/teamextension/homebrew-updatedep"
-  url "https://github.com/teamextension/homebrew-updatedep/releases/download/1.0.0/updatedep.jar"
-  sha256 "6EFD78A45B9EEB2C09838CA5DB7E77816806BB5CED8DFAADA812865001D82ADE"
+  url "https://github.com/teamextension/homebrew-updatedep/releases/download/1.1.0/updatedep.jar"
+  sha256 "D834F95D657B3BFBDECC9475BFD99266E415BCCE652BE111ED06F89F0DB82D02"
   license "MIT"
 
   depends_on "openjdk"
