7353a5b95b02eeecc0ab2c0821d396cc59080158
jesli6991 <119661452+jesli6991@users.noreply.github.com>
2025-04-14 17:27:25 -0400

    Update Updatedep.rb

--- Formula/Updatedep.rb
+++ Formula/Updatedep.rb
@@ -2,14 +2,14 @@ class Updatedep < Formula
   desc "Updatedep is an application that analyzes the dependencies of a project, this helps check what dependency versions can be used with less to no vulnerabilities."
   homepage "https://github.com/teamextension/updatedep"
   url "https://github.com/teamextension/updatedep/releases/download/1.3.0/updatedep.jar"
-  sha256 "498B3AC532646CE4DA76F3CB5FDC7DC722D84ACD725386FE68DF95A2617AD4B1"
+  sha256 "081b7458c989bcc1f377989f40293fafb4d02b56cf077503835abe705dcd9bf5"
   license "MIT"
 
   depends_on "openjdk@11"
 
   resource "exclude" do
     url "https://raw.githubusercontent.com/teamextension/updatedep/main/exclude.txt"
-    sha256 "bfd021a1d27256daf2f915717f3201c43955fe5ac28ef308b34a3dc2c6be8c0d"
+    sha256 "fcf67c8848697f25a6ada9984aff5a43a9e10cc36ed2daf1a7392b2d24b44c71"
   end
 
   def install
